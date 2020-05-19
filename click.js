c81ceb2e3637900b52f01d76ebf23ee66282d675
matthew.sanii <matthew.sanii@mths.ca>
2020-05-19 13:35:56 +0000

    edited index.html, click.js

--- click.js
+++ click.js
@@ -14,13 +14,13 @@ function Click(){
   let answer2=document.getElementById("answer2");
   answer2=Number(answer2.value);
   if (answer2 > 4){
-    response.innerHTML="Please input a number of toppings between 0 and 4."; // This innerHTML line returns that the discount is available.
+    response.innerHTML="Please input a number of toppings between 0 and 4."; // This innerHTML line asks you to input a topping number between 0 and 4.
   }
-  else if (answerValue == "Large" || answerValue == "large"){ // This if statement checks to see if you chose to order a large pizza.
-    if (answer2 == 0){
+  else if (answerValue=="Large" || answerValue=="large"){ // This if statement checks to see if you chose to order a large pizza.
+    if (answer2==0){
       toppings=0;
     }
-    else if (answer2 == 4){
+    else if (answer2==4){
       toppings=3.35;
     }
     else{
@@ -29,15 +29,15 @@ function Click(){
     cost=Number(6+toppings);
     taxes=Number(cost*0.13);
     totalcost=Number(cost+taxes);
-    response.innerHTML=cost.toFixed(2); // This innerHTML line returns that the discount is available.
-    tax.innerHTML=taxes.toFixed(2);
-    total.innerHTML=totalcost.toFixed(2);
+    response.innerHTML=cost.toFixed(2); // This innerHTML line returns the cost of the pizza without tax.
+    tax.innerHTML=taxes.toFixed(2); // This innerHTML line returns the tax amount.
+    total.innerHTML=totalcost.toFixed(2); // This innerHTML line returns the cost of the pizza with tax.
 }
-  else if (answerValue == "Extra Large" || answerValue == "extra large" || answerValue == "Extra large" || answerValue == "extra Large"){ // This if statement checks to see if you chose to order a large pizza.
-    if (answer2 == 0){
+  else if (answerValue=="Extra Large" || answerValue=="extra large" || answerValue=="Extra large" || answerValue=="extra Large"){ // This if statement checks to see if you chose to order an extra large pizza.
+    if (answer2==0){
       toppings=0;
     }
-    else if (answer2 == 4){
+    else if (answer2==4){
       toppings=3.35;
     }
     else{
@@ -46,8 +46,8 @@ function Click(){
     cost=Number(10+toppings);
     taxes=Number(cost*0.13);
     totalcost=Number(cost+taxes);
-    response.innerHTML=cost.toFixed(2); // This innerHTML line returns that the discount is available.
-    tax.innerHTML=taxes.toFixed(2);
-    total.innerHTML=totalcost.toFixed(2);
+    response.innerHTML=cost.toFixed(2); // This innerHTML line returns the cost of the pizza without tax.
+    tax.innerHTML=taxes.toFixed(2); // This innerHTML line returns the tax amount.
+    total.innerHTML=totalcost.toFixed(2); // This innerHTML line returns the cost of the pizza with tax.
 }
 }
