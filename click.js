20a3d961cf2e878d9c3be3541dc54b07c5c6c70c
matthew.sanii <matthew.sanii@mths.ca>
2020-05-19 13:30:48 +0000

    edited index.html, click.js

--- click.js
+++ click.js
@@ -4,13 +4,16 @@ let tax=document.getElementById("tax");
 let total=document.getElementById("total");
 document.getElementById("Button").onclick=function(){Click()};
 function Click(){
+  let cost=0;
+  let taxes=0;
+  let totalcost=0;
   let toppings=0;
   let answer=document.getElementById("answer");
   let answerValue;
-  answerValue = String(answer.value);
+  answerValue=String(answer.value);
   let answer2=document.getElementById("answer2");
-  answer2 = Number(answer2.value);
-  if ((answer2 > 4) || (answerValue )){
+  answer2=Number(answer2.value);
+  if (answer2 > 4){
     response.innerHTML="Please input a number of toppings between 0 and 4."; // This innerHTML line returns that the discount is available.
   }
   else if (answerValue == "Large" || answerValue == "large"){ // This if statement checks to see if you chose to order a large pizza.
@@ -23,8 +26,28 @@ function Click(){
     else{
       toppings=(answer2*0.75)+0.25;
     }
-  response.innerHTML=toppings; // This innerHTML line returns that the discount is available.
-  tax.innerHTML="";
-  total.innerHTML="";
+    cost=Number(6+toppings);
+    taxes=Number(cost*0.13);
+    totalcost=Number(cost+taxes);
+    response.innerHTML=cost.toFixed(2); // This innerHTML line returns that the discount is available.
+    tax.innerHTML=taxes.toFixed(2);
+    total.innerHTML=totalcost.toFixed(2);
+}
+  else if (answerValue == "Extra Large" || answerValue == "extra large" || answerValue == "Extra large" || answerValue == "extra Large"){ // This if statement checks to see if you chose to order a large pizza.
+    if (answer2 == 0){
+      toppings=0;
+    }
+    else if (answer2 == 4){
+      toppings=3.35;
+    }
+    else{
+      toppings=(answer2*0.75)+0.25;
+    }
+    cost=Number(10+toppings);
+    taxes=Number(cost*0.13);
+    totalcost=Number(cost+taxes);
+    response.innerHTML=cost.toFixed(2); // This innerHTML line returns that the discount is available.
+    tax.innerHTML=taxes.toFixed(2);
+    total.innerHTML=totalcost.toFixed(2);
 }
 }
